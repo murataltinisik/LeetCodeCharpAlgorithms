f28c33b2798d83f6cf0fe135cea712296dcb57eb
Murat Altınışık <altinisik.murat571@gmail.com>
2025-05-17 08:12:17 +0300

    Completed Charp Algoritms and "TwoSum"

---
+++ TwoSum/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] TwoSum(int[] nums, int target)
+{
+   // Iterate through each number in the array
+   for (int i = 0; i < nums.Length; i++)
+   {
+      for (int j = i; j < nums.Length; j++)
+      {
+         if(i != j){
+            // Check if their sum matches the target
+            if (nums[i] + nums[j] == target)
+            {
+               // Store the matching numbers
+               return new int[] { i, j };
+            }
+         }
+      }
+   }
+ 
+   return new int[0];
+}
+
+int[] nums = new int[3] { 2, 3, 3 };
+int[] result = TwoSum(nums, 6);
+
+Console.WriteLine($"[{result[0]}, {result[1]}]");
