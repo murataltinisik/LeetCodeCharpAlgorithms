4b3fa6a59dbabad49d403c501774a404df18146e
Murat Altınışık <altinisik.murat571@gmail.com>
2025-05-17 08:33:46 +0300

    Completed IsPalindrome

---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,17 @@
+﻿bool IsPalindrome(int x)
+{
+    // Int To String
+    char[] xValues = x.ToString().ToCharArray();
+    
+    // New Number
+    string newNumber = string.Empty;
+    
+    // Create Reverse Number
+    for (int i = xValues.Length - 1; i >= 0; i--)
+        newNumber += xValues[i].ToString();
+    
+    if(newNumber == x.ToString()) return true;
+    return false;
+}
+
+Console.WriteLine($"{IsPalindrome(11)}");
