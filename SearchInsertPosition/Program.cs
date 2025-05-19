1a6783a5f3b4c499a1406aa1b16070b26314807b
Murat Altınışık <altinisik.murat571@gmail.com>
2025-05-19 22:28:26 +0300

    Search Insert Position

---
+++ SearchInsertPosition/Program.cs
@@ -0,0 +1,45 @@
+﻿int SearchInsert(int[] nums, int target)
+{
+    // Variables
+    int searchIndex = -1;
+    
+    // Max Index
+    searchIndex = target > nums[nums.Length - 1] ? nums.Length : -1;
+
+    if(searchIndex == -1){
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] == target) searchIndex = i;
+
+            if(nums[i] < target && nums[i + 1] > target) searchIndex = i + 1;
+        }
+    }
+    
+    return searchIndex == -1 ? 0 : searchIndex;
+}
+
+/*
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+   You must write an algorithm with O(log n) runtime complexity.
+
+   Example 1:
+   Input: nums = [1,3,5,6], target = 5
+   Output: 2
+
+   Example 2:
+   Input: nums = [1,3,5,6], target = 2
+   Output: 1
+
+   Example 3:
+   Input: nums = [1,3,5,6], target = 7
+   Output: 4
+ */
+
+int[] array = { 1, 3, 5, 6 };
+Console.WriteLine("---------------");
+Console.WriteLine(SearchInsert(array, 5));
+Console.WriteLine("---------------");
+Console.WriteLine(SearchInsert(array, 2));
+Console.WriteLine("---------------");
+Console.WriteLine(SearchInsert(array, 7));
+Console.WriteLine("---------------");
---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,44 @@
+﻿bool IsValid(string s)
+{
+    foreach (var c in s)
+    {
+        // Char Code
+        int charCode = c;
+        
+        //
+        if (charCode == 40)
+        {
+            
+        }
+    }
+    
+    return false;
+}
+
+/*
+ * ( -> 40
+ * ) -> 41
+ *
+ * { -> 123
+ * } -> 125
+ * 
+ * [ -> 91
+ * ] -> 93
+ * 
+ * 
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+   
+   An input string is valid if:
+   
+   Open brackets must be closed by the same type of brackets.
+   Open brackets must be closed in the correct order.
+   Every close bracket has a corresponding open bracket of the same type.
+ */
+
+Console.WriteLine(IsValid("()"));
+Console.WriteLine("-------------");
+Console.WriteLine(IsValid("()[]{}"));
+Console.WriteLine("-------------");
+Console.WriteLine(IsValid("(]"));
+Console.WriteLine("-------------");
+Console.WriteLine(IsValid("([])"));
