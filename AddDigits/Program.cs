ffd5e8a3fb6eb344a27e6dc3de62547fe8c2168e
Murat Altınışık <altinisik.murat571@gmail.com>
2025-05-21 08:26:08 +0300

    Add Digits

---
+++ AddDigits/Program.cs
@@ -0,0 +1,10 @@
+﻿int AddDigits(int num)
+{
+    if (num == 0) return 0;
+    return 1 + (num - 1) % 9;
+}
+
+Console.WriteLine("------------");
+Console.WriteLine(AddDigits(38));
+Console.WriteLine("------------");
+Console.WriteLine(AddDigits(0));
