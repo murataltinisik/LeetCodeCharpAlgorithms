a89d72644d6b03c417040f9225e1a2691b57d70c
Murat Altınışık <altinisik.murat571@gmail.com>
2025-05-21 21:06:22 +0300

    Find Intersection

---
+++ FindIntersection/Program.cs
@@ -0,0 +1,25 @@
+﻿string FindIntersection(string[] strArr) {
+    // Split Arrays
+    string finded = string.Empty;
+    string[] array = strArr[0].Split(",");
+    string[] searching = strArr[1].Split(",");
+
+    for (int i = 0; i < array.Length; i++)
+        for (int j = 0; j < searching.Length; j++)
+            if (array[i] == searching[j]) {
+                finded += array[i] + (i == searching.Length - 1 ? "" : ",");
+            }
+
+    return finded;
+}
+
+
+// Array
+string[] array = new string[] { "1, 3, 4, 7, 13", "1, 2, 4, 13, 15" };
+string[] array2 = new string[] { "1, 3, 9, 10, 17, 18", "1, 4, 9, 10" };
+
+Console.WriteLine("----------");
+Console.WriteLine(FindIntersection(array));
+Console.WriteLine("----------");
+Console.WriteLine(FindIntersection(array2));
+Console.WriteLine("----------");
