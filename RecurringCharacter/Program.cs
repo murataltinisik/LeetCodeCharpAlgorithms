e80a8482c9d90e6ca676f9809e128320b41b4636
Murat Altınışık <altinisik.murat571@gmail.com>
2025-05-22 09:19:27 +0300

    Recurring Character

---
+++ RecurringCharacter/Program.cs
@@ -0,0 +1,24 @@
+﻿string RecurringCharacter(string str)
+{
+    string result = "none";
+    
+    for (int i = 0; i < str.Length; i++)
+    {
+       // Temp
+       string temp = str.Substring(i, 1);
+
+       // For Loop
+       for (int j = i + 1; j < str.Length; j++)
+       {
+           if(temp == str[j].ToString())
+               result = "true";
+       }
+    }
+
+    return result;
+}
+
+Console.WriteLine("------------");
+Console.WriteLine(RecurringCharacter("abca"));
+Console.WriteLine("------------");
+Console.WriteLine(RecurringCharacter("abcdef"));
