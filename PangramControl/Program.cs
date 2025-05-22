97abd0282a7e2209b1e61f30d7c414db7afbf309
Murat Altınışık <altinisik.murat571@gmail.com>
2025-05-22 09:46:30 +0300

    Pangram Control

---
+++ PangramControl/Program.cs
@@ -0,0 +1,35 @@
+﻿string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+bool PangramControl(string input)
+{
+    // Variables
+    int counter = 0;
+    string value = input.ToLower();
+    
+    // For
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        // Is Found
+        bool isFound = false;
+
+        // For
+        for (int j = 0; j < value.Length; j++)
+            if (alphabet[i] == value[j])
+            {
+                // Is Found
+                isFound = true;
+                
+                // Break;
+                break;
+            }
+        
+        if (isFound) counter++;
+    }
+    
+    return counter == alphabet.Length;
+}
+
+Console.WriteLine("-------------");
+Console.WriteLine(PangramControl("The quick brown fox jumps over the lazy dog"));
+Console.WriteLine("-------------");
+Console.WriteLine(PangramControl("Hello world"));
